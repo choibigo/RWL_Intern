a6afdb7df1265a11ace08420731d4749185ed184
Slberianhusky1 <gjhmanse@hanyang.ac.kr>
2024-05-11 22:39:33 +0900

    conda, json, excel md파일 생성
    회사 정보 json 파일로 저장
    
    html 에서 json을 가져와 사이트 생성하려 했으나 출력 실패..

--- docs/md/junhea_workspace/crawling/new_ver/naver_stock.py
+++ docs/md/junhea_workspace/crawling/new_ver/naver_stock.py
@@ -1,10 +1,18 @@
 import json
+from openpyxl import load_workbook
 import urllib.request
 
-item_code = "373220"
-url = "https://m.stock.naver.com/api/stock/%s/integration"%(item_code)
+wb = load_workbook('C:/anaconda3 for study/gitgit/RWL_Intern/docs/md/junhea_workspace/crawling/new_ver/company_code.xlsx')
 
-raw_data = urllib.request.urlopen(url).read()
-json_data = json.loads(raw_data)
+data = wb.active
 
-print(json_data)
+all_data = {}
+for i in range(954):
+    item_code = data['A'+str(i+2)].value
+    url = "https://m.stock.naver.com/api/stock/%s/integration"%(item_code)
+    raw_data = urllib.request.urlopen(url).read()
+    json_data = json.loads(raw_data)
+    all_data[i] = json_data
+
+with open('C:/anaconda3 for study/gitgit/RWL_Intern/docs/md/junhea_workspace/crawling/new_ver/company_name.json', 'a') as f:
+    json.dump(all_data, f, indent=4)
