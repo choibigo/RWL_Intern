456b08361dee9147b3f6de0e577938ca46470f17
Theo Choi <tpingouin@gmail.com>
2026-02-10 18:24:23 +0900

    Python: Jump to python 2

---
+++ to_workspace/python/etc/jump_to_python_test.py
@@ -0,0 +1,20 @@
+multiline='''
+Life is too short
+You need python
+'''
+
+print(multiline)
+
+print(f"안녕, 지금은 정렬 연습 중이야 {"hi":>10}")
+print(f"안녕, 지금은 정렬 연습 중이야 {"hi":*^20}")
+
+
+a = "Life is too short"
+print(a.replace("Life", "Your leg"))
+print(a)
+
+a = []
+
+
+a = list()
+print(a)
